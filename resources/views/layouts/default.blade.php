8aa32893965da2e30e47bee945df45e38431a81b
juran <965569983@qq.com>
2018-12-20 11:32:55 +0800

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title', 'Lamei')</title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('title', '')
+
+@section('content')
+  <h1>帮助</h1>
+@stop
